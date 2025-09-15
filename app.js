604095c50c827bde89201e9a750dd2ae1474039b
Jmoney <67088606+AsanroJay@users.noreply.github.com>
2025-09-15 13:51:16 +0800

    refactor: changed folder name to views for MVC

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000
 
 // Templates and Static Files
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'templates'));
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Routes
