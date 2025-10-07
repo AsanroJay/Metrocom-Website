535aaada55a54be2ce1914f670b244763e951177
Jmoney <67088606+AsanroJay@users.noreply.github.com>
2025-10-08 00:31:53 +0800

    chore: add sample products for manageProducts page

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -5,4 +5,32 @@ router.get('/quota', (req, res) =>{
     res.render('quota')
 });
 
+
+// TEMP PRODUCTS
+const sampleProducts = [
+  {
+    _id: "1",
+    name: "Metrocom Office Chair",
+    sku: "MC-CHAIR-001",
+    image: "/assets/logo.png"
+  },
+  {
+    _id: "2",
+    name: "Metrocom Desk Lamp",
+    sku: "MC-LAMP-002",
+    image: "/assets/logo.png"
+  },
+  {
+    _id: "3",
+    name: "Metrocom Filing Cabinet",
+    sku: "MC-CAB-003",
+    image: "/assets/logo.png"
+
+  }
+];
+
+router.get('/products',(req, res) => {
+    res.render('manageProducts', { products: sampleProducts})
+});
+
 module.exports = router;
