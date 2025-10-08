6818c578b8d2365059fccef820dfbedfcde92771
Jmoney <67088606+AsanroJay@users.noreply.github.com>
2025-10-08 18:22:34 +0800

    chore: pass sample products to quota.ejs

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/quota', (req, res) =>{
-    res.render('quota')
+    res.render('quota', { products: sampleProducts})
 });
 
 
